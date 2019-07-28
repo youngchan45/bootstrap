aa39e0a91d09bd500c4907bee07b20cefe9710f7
youngchan45 <youngchan93@163.com>
2019-07-28 14:51:59 +0800

    客服对话框按钮切换

--- steer/src/index.js
+++ steer/src/index.js
@@ -1,15 +1,26 @@
 
-$(document).ready(function(){
-    // function hidedia(){
-    //     $("#dia-cli").hide();
-    // }
-    $("#dia").click(function(){
-        $("#dia-cli").toggle();
-        $("#more").attr("src")=="img/关闭.svg";
-    });
-    $("#dia-cli").on("show",function(){
-        // $("#more").attr("src")=="../img/关闭.svg";
-    })
+$(document).ready(function () {
+    // $("#dia").click(function () { 
+    //     $("#dia-cli").toggle();
+    //     $("#dia-img").attr("src","../img/关闭.svg");
 
+    // });
+    //  $("#dia-img").click(function(){
+    //     $("#dia-cli").hide();
+    //     $("#dia-img").attr("src","../img/聊天记录.svg");
+    //  })
+    // $("#dia-cli").hide(function(){
+    //     $("#dia-img").attr("src","../img/聊天记录.svg");
+    // })
+    $("#dia").click(function () {
+        $("#dia-cli").toggle();
+
+        if ($(this).find("#dia-img").attr("src") == "../img/聊天记录.svg") {
+            // $("#dia-cli").show();
+            $(this).find("#dia-img").attr("src", "../img/关闭.svg")
+        } else {
+            $(this).find("#dia-img").attr("src", "../img/聊天记录.svg")
+        }
+    })
 
 });
